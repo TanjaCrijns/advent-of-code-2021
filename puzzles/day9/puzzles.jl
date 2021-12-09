d165cad2d688c84546f8e2e84d198d835dd359b3
Tanja Crijns <tanja.crijns@rtl.nl>
2021-12-09 13:30:30 +0100

    Add solution day 9

---
+++ puzzles/day9/puzzles.jl
@@ -0,0 +1,71 @@
+#!/usr/bin/julia
+include("../../utils.jl")
+
+function checkAdjacentLocations(x,y,heightMmap)
+    neighbors = [(-1,0), (0, -1), (1,0), (0,1)]
+    higherNeighbors = 0
+    for neighbor in neighbors
+        if x + neighbor[1] < 1 || x + neighbor[1] > 100 || y + neighbor[2] < 1 || y + neighbor[2] > 100
+            higherNeighbors += 1
+        else
+            if heightMmap[x+neighbor[1], y+neighbor[2]] > heightMmap[x,y]
+                higherNeighbors += 1
+            end
+        end
+    end
+    return (higherNeighbors==4, heightMap[x,y]+1)
+end
+
+function getBasinSize(x,y,heightMmap)
+    toCheck = [(x,y)]
+    found = []
+    neighbors = [(-1,0), (0, -1), (1,0), (0,1)]
+
+    while length(toCheck) > 0
+        for neighbor in neighbors
+            tempX, tempY = toCheck[1][1], toCheck[1][2]
+            if tempX + neighbor[1] < 1 || tempX + neighbor[1] > 100 || tempY + neighbor[2] < 1 || tempY + neighbor[2] > 100
+            else
+                if heightMmap[tempX+neighbor[1], tempY+neighbor[2]] != 9
+                    if (tempX+neighbor[1], tempY+neighbor[2]) ∉ found
+                        push!(found, (tempX+neighbor[1], tempY+neighbor[2]))
+                        push!(toCheck, (tempX+neighbor[1], tempY+neighbor[2]))
+                    end
+                end
+            end
+        end
+        popfirst!(toCheck)
+    end
+    return length(found)
+end
+
+### Create heightmap matrix ###
+input = partseToString(readInput("input.txt"))
+heightMap = zeros(Int, 100,100)
+for i in 1:length(input)
+    for j in 1:length(input)
+        heightMap[i,j] = parse(Int,input[i][j])
+    end
+end
+
+### Find lowpoints ###
+risks = []
+lowPoints = []
+for i in 1:length(input)
+    for j in 1:length(input)
+        lowPoint, risklevel = checkAdjacentLocations(i,j,heightMap)
+        if lowPoint
+            push!(risks, risklevel)
+            push!(lowPoints, (i,j))
+        end
+    end
+end
+
+### Calculate basins ###
+basinSizes = []
+for lowPoint in lowPoints
+    push!(basinSizes, getBasinSize(lowPoint[1], lowPoint[2], heightMap))
+end
+
+print("The answer to part 1 is: ", sum(risks), '\n')
+print("The answer to part 2 is: ", reduce(*, sort(basinSizes)[end-2:end]), '\n')
