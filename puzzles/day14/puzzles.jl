14b24c8cbba570f250952df125d51bfb1cdbdb0a
Tanja Crijns <tanja.crijns@rtl.nl>
2021-12-14 08:33:30 +0100

    Update print statements

---
+++ puzzles/day14/puzzles.jl
@@ -0,0 +1,13 @@
+#!/usr/bin/julia
+include("../../utils.jl")
+
+
+input = partseToString(readInput("input.txt"))
+template = input[1]
+rules = input[3:end]
+
+println(template)
+
+println("The answer to part 1 is: ", "")
+println("The answer to part 2 is: ", "")
+
